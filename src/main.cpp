23ba39d8e220c38b3558b90d62ac57fbb12cd46a
Ilhom77 <103019100+Ilhom77@users.noreply.github.com>
2022-05-01 14:47:44 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 // Copyright 2022 NNTU-CS
-#include "tpqueue.h"
-
-int main() {
-    TPQueue<SYM, 5> pqueue;
-    pqueue.push(SYM{'a', 4});
-    pqueue.push(SYM{'b', 7});
-    SYM c1 = pqueue.pop();
-    SYM c2 = pqueue.pop();
-    return 0;
-}
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+template <typename T, int Size>
+class TPQueue {
+ private:
+  T arr[Size];
+  int n = 0;
+ public:
+  void push(T item) {
+    if (n >= Size) throw "full!"
+    else for (int i = n++; i >= 0; i--)
+    for (int i = n++; i >= 0; i--)
+      if (i > 0 && arr[i - 1].prior >= item.prior) {
+        arr[i] = arr[i - 1];
+      } else {
+        arr[i] = item;
+        break;
+      }
+  }
+  T pop() {
+    if (n <= 0) throw "empty!";
+    return arr[--n];
+  }
+};
+struct SYM {
+  char ch;
+  int prior;
+};
