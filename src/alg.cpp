9d9461c7bde723b6b3c165067da4e2ad9263344c
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-05-26 12:52:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,27 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+template<typename T, int size>
+class TPQueue {
+ private:
+     T a[100];
+     int begin;
+     int end;
+ public:
+     TPQueue() : begin(0), end(0) { }
+     void push(T x) {
+         int cif = end++;in <= --cif);) {
+         for (cif; (begin <= --cif) && (x.prior > a[cif % size].prior);) {
+             a[(cif + 1) % size] = a[cif % size];
+         }
+         a[(cif + 1) % size] = x;
+     }
+     T pop() {
+         return a[(begin++) % size];
+     }
+};
+struct SYM {
+  char ch;
+  int prior;
+};
+#endif  // INCLUDE_TPQUEUE_H_
