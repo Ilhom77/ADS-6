6308e45c0289141384807ae07151db7c275bc789
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-05-26 12:42:42 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,58 @@
 #define INCLUDE_TPQUEUE_H_
 template <typename T, int Size>
 class TPQueue {
+  // реализация шаблона очереди с приоритетом на кольцевом буфере
  private:
-  T arr[Size];
+  T a[Size];
   int n = 0;
+
  public:
   void push(T item) {
-    if (n >= Size) throw "full!"
-    else for (int i = n++; i >= 0; i--)
-    for (int i = n++; i >= 0; i--)
-      if (i > 0 && arr[i - 1].prior >= item.prior) {
-        arr[i] = arr[i - 1];
-      } else {
-        arr[i] = item;
-        break;
-      }
+  if (n >= Size) throw "full";
+  for (int i = n++; i >= 0; i--)
+  if ((i > 0) && (a[i - 1].prior >= item.prior)) {
+  a[i] = a[i - 1];
+  } else {
+  a[i] = item;
+  break;
+  }
   }
   T pop() {
-    if (n <= 0) throw "empty!";
-    return arr[--n];
+  if (n <= 0) throw "empty";
+  return a[--n];
   }
 };
+
 struct SYM {
   char ch;
   int prior;
 };
+#endif  // INCLUDE_TPQUEUE_H_
+template <typename T, int Size>
+class TPQueue {
+ private:
+  T a[Size];
+  int n = 0;
+
+ public:
+  void push(T item) {
+  if (n >= Size) throw "full";
+  for (int i = n++; i >= 0; i--)
+  if ((i > 0) && (a[i - 1].prior >= item.prior)) {
+  a[i] = a[i - 1];
+  } else {
+  a[i] = item;
+  break;
+  }
+  }
+  T pop() {
+  if (n <= 0) throw "empty";
+  return a[--n];
+  }
+};
+
+struct SYM {
+  char ch;
+  int prior;
+};
+#endif  // INCLUDE_TPQUEUE_H_
