0c75754df8dbd52e464e884c703a7560de96e117
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-05-29 11:16:36 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,28 +1,2 @@
 
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
-
-template<typename T, int size>
-class TPQueue {
- private:
-    T arr[size] = {0};
-    int v = 0, b = 0;
- public:
-  void push(T value) {
-      int c = b;
-      while ((--c >= v) && (value.prior > arr[c % size].prior)) {
-          arr[(c + 1) % size] = arr[c % size];
-      }
-      arr[(c+1) % size] = value;
-      b++;
-  }
-  T pop() {
-      return arr[(v++) % size];
-  }
-};
-struct SYM {
-  char ch;
-  int prior;
-};
-#endif  // INCLUDE_TPQUEUE_H_
+// Copyright 2022 NNTU-CS#ifndef INCLUDE_TPQUEUE_H_#define INCLUDE_TPQUEUE_H_#include <string>template<typename T, int size>class TPQueue { private: T *arr; int last; public: TPQueue(); void push(const T&); T pop();};struct SYM { char ch; int prior;};template<typename T, int size>TPQueue<T, size>::TPQueue() : last(-1) { arr = new T[size]; }template<typename T, int size>T TPQueue <T, size> ::pop() { return arr[last--];}template<typename T, int size>void TPQueue<T, size>::push(const T& item) { if (last >= 0) { for (int i = 0; i <= last; i++) { if (arr[i].prior >= item.prior) { if (last >= (size - 1)) { throw std::string("Full!"); } else { last++; for (int j = last; j > i; j--) { arr[j] = arr[j - 1]; } arr[i] = item; } break; } else if (i == last) { last++; arr[last] = item; break; } } } else { last = 0; arr[last] = item; }}#endif // INCLUDE_TPQUEUE_H_
